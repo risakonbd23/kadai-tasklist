0b021be3615637d676f19beebc0d70b52a9b567c
risako.hamba <risako.hamba@rakuten.com>
2018-06-12 07:09:17 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'TasksController@index');
+Route::get('/', 'welcome.blade');
 Route::resource('tasks', 'TasksController');
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
